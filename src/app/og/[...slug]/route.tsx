ed023f9d8262bc3089f087e332e8d78ae1c60196
BBAKJUN <wnsguddl789@gmail.com>
2024-09-27 10:57:58 +0900

    feat: 폰트 최적화

--- src/app/og/[...slug]/route.tsx
+++ src/app/og/[...slug]/route.tsx
@@ -9,28 +9,19 @@ export async function GET(_req: Request, { params }: { params: { slug: string[] 
 
   return new ImageResponse(
     (
-      <div tw="flex p-10 h-full w-full bg-white flex-col" style={font('Inter 300')}>
+      <div tw="flex p-10 h-full w-full bg-white flex-col">
         <header tw="flex text-[36px] w-full">
-          <div tw="font-bold" style={font('Inter 600')}>
-            Frontend Engineer - 박준형
-          </div>
+          <div tw="font-bold">Frontend Engineer - 박준형</div>
           <div tw="grow" />
           <div tw="text-[28px]">dev-bbak.site</div>
         </header>
 
         <main tw="flex grow pb-3 flex-col items-center justify-center">
           <div tw="flex">
-            <div
-              tw="bg-gray-100 p-8 text-7xl font-medium rounded-md text-center"
-              style={font('Inter 500')}
-            >
-              {post.title}
-            </div>
+            <div tw="bg-gray-100 p-8 text-7xl font-medium rounded-md text-center">{post.title}</div>
           </div>
 
-          <div tw="mt-5 flex text-3xl text-gray-500" style={font('Roboto Mono 400')}>
-            {post.date}
-          </div>
+          <div tw="mt-5 flex text-3xl text-gray-500">{post.date}</div>
         </main>
       </div>
     ),
@@ -39,9 +30,4 @@ export async function GET(_req: Request, { params }: { params: { slug: string[] 
       height: 630,
     }
   )
-}
-
-// lil helper for more succinct styles
-function font(fontFamily: string) {
-  return { fontFamily }
 }
