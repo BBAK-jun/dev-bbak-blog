79d9e0b32eb027f16cc4dd059997756dba14729e
BBAK_JUN <33216504+wnsguddl789@users.noreply.github.com>
2024-09-27 14:54:15 +0900

    Develop (#4)
    * 병합 (#3)
    
    * 릴리즈
    
    * feat: 이직썰
    
    * feat: 글 퇴고
    
    * og image
    
    * feat: 글꼴 줄이기
    
    * feat: 폰트 최적화
    
    * feat: 폰트 최적화
    
    * feat: 엣지런타임 최적화

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,16 @@
+import { genPageMetadata } from 'src/app/seo'
+import { BlockMapType, NotionRenderer } from 'react-notion'
+import 'react-notion/src/styles.css'
+import 'prismjs/themes/prism-tomorrow.css'
+
+export const revalidate = 3600
+
+export const metadata = genPageMetadata({ title: 'Resume' })
+
+export default async function ResumePage() {
+  const blockMap: BlockMapType = await fetch(
+    `https://notion-api.splitbee.io/v1/page/${process.env.NEXT_PUBLIC_NOTION_RESUME}`
+  ).then((res) => res.json())
+
+  return <NotionRenderer blockMap={blockMap} />
+}
---
+++ src/components/Posts.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import { getPosts } from 'actions/get-posts'
+import { Blog } from 'contentlayer/generated'
+import siteMetadata from 'data/siteMetadata'
+import Link from 'next/link'
+import { formatDate } from 'pliny/utils/formatDate'
+import { useCallback, useEffect, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
+import Tag from './Tag'
+
+type PostsProps = {
+  initialPosts: Blog[]
+}
+
+export default function Posts({ initialPosts }: PostsProps) {
+  const [posts, setPosts] = useState(initialPosts)
+  const [page, setPage] = useState(1)
+  const [limit, setLimit] = useState(10)
+  const [hasMore, setHasMore] = useState(true)
+
+  const { ref, inView } = useInView()
+
+  const loadMorePosts = useCallback(async () => {
+    const morePosts = await getPosts({ page, limit })
+
+    setPosts([...posts, ...morePosts.data])
+    setPage(morePosts.currentPage + 1)
+    setHasMore(morePosts.hasMore)
+  }, [page, posts])
+
+  useEffect(() => {
+    if (inView) {
+      loadMorePosts()
+    }
+  }, [inView, loadMorePosts])
+
+  return (
+    <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+      {posts.map(({ slug, date, title, summary, tags, readingTime }) => (
+        <li key={slug} className="py-12">
+          <article>
+            <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+              <dl>
+                <dt className="sr-only">Published on</dt>
+                <dd className="text-base font-medium leading-6 text-black-500 dark:text-white-400">
+                  <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+                </dd>
+                <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                  0 views / {readingTime.text}
+                </dd>
+              </dl>
+              <div className="space-y-5 xl:col-span-2">
+                <div className="space-y-6">
+                  <div>
+                    <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                      <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+                        {title}
+                      </Link>
+                    </h2>
+                    <div className="flex flex-wrap">
+                      {tags.map((tag) => (
+                        <Tag key={tag} text={tag} />
+                      ))}
+                    </div>
+                  </div>
+                  <div className="prose max-w-none text-gray-500 dark:text-gray-400">{summary}</div>
+                </div>
+                <div className="text-base font-medium leading-6">
+                  <Link
+                    href={`/blog/${slug}`}
+                    className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                    aria-label={`Read "${title}"`}
+                  >
+                    Read more &rarr;
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </article>
+        </li>
+      ))}
+
+      {hasMore && <div ref={ref}>Loading...</div>}
+    </ul>
+  )
+}
