7563dc25b6bdbca0a9904f7440e6f12653caafb1
BBAKJUN <wnsguddl789@gmail.com>
2024-09-27 15:24:20 +0900

    feat: og image 디자인 변경

--- src/app/og/[...slug]/route.tsx
+++ src/app/og/[...slug]/route.tsx
@@ -1,33 +1,20 @@
 export const runtime = 'edge'
 
 import { ImageResponse } from 'next/og'
+import BlogOGImage from 'components/OpenGraphImage'
 
-export async function GET(request: Request) {
+export async function GET(request: Request, { params }: { params: { slug: string[] } }) {
   const { searchParams } = new URL(request.url)
   const title = searchParams.get('title')
   const date = searchParams.get('date')
+  const category = searchParams.get('category')
 
-  return new ImageResponse(
-    (
-      <div tw="flex p-10 h-full w-full bg-white flex-col">
-        <header tw="flex text-[36px] w-full">
-          <div tw="font-bold">Frontend Engineer - 박준형</div>
-          <div tw="grow" />
-          <div tw="text-[28px]">dev-bbak.site</div>
-        </header>
+  if (!title || !date || !category) {
+    return new Response('Missing title or date query parameter', { status: 400 })
+  }
 
-        <main tw="flex grow pb-3 flex-col items-center justify-center">
-          <div tw="flex">
-            <div tw="bg-gray-100 p-8 text-7xl font-medium rounded-md text-center">{title}</div>
-          </div>
-
-          <div tw="mt-5 flex text-3xl text-gray-500">{date}</div>
-        </main>
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    }
-  )
+  return new ImageResponse(<BlogOGImage title={title} date={date} category={category} />, {
+    width: 1200,
+    height: 630,
+  })
 }
