dfec6cd44a205ef938aec60aab24ed5f8365572e
BBAKJUN <wnsguddl789@gmail.com>
2024-10-14 13:10:02 +0900

    feat: useSyncExternalStore 퍼블리시

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const ContentSecurityPolicy = `
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src giscus.app youtube.com www.youtube.com
+  frame-src giscus.app youtube.com www.youtube.com https://codesandbox.io/
 `
 
 const securityHeaders = [
