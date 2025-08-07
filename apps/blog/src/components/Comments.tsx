e2c9508c0c2d193404e62abe14c058325636a988
BBAK_JUN <wnsguddl789@gmail.com>
2025-08-07 15:24:04 +0900

    fix: Comments 컴포넌트에서 slug 경로를 'blog/${slug}'에서 '${slug}'로 수정하고, 콘솔 로그 추가로 디버깅 정보 제공

--- apps/blog/src/components/Comments.tsx
+++ apps/blog/src/components/Comments.tsx
@@ -4,7 +4,8 @@ import siteMetadata from 'data/siteMetadata'
 import { Comments as CommentsComponent } from 'pliny/comments'
 
 const Comments = ({ slug }: { slug: string }) => {
-  return siteMetadata.comments && <CommentsComponent commentsConfig={siteMetadata.comments} slug={`blog/${slug}`} />
+  console.log(siteMetadata.comments, slug)
+  return siteMetadata.comments && <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
 }
 
 export default Comments
